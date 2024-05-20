d45bf4e210314d48358d6d1ce919ab0e9c8bb065
Vinny Green <vinnysgreen@gmail.com>
2024-05-20 12:04:15 -0700

    Update file

--- wp-content/plugins/newsplicity/settings/front-page/init.php
+++ wp-content/plugins/newsplicity/settings/front-page/init.php
@@ -4,8 +4,6 @@ add_filter('mb_settings_pages', function ($settings_pages) {
 	$settings_pages[] = [
 		'menu_title' => __('Front Page', 'newsplicity'),
 		'id' => 'newsplicity_front_page_settings',
-		'position' => 25,
-		'columns' => 1,
 	];
 
 	return $settings_pages;
@@ -16,7 +14,7 @@ add_filter('rwmb_meta_boxes', function ($meta_boxes) {
 	$prefix = 'newsplicity_';
 
 	$meta_boxes[] = [
-		'title' => __('Front Page', 'newsplicity'),
+		'title' => __('Settings', 'newsplicity'),
 		'settings_pages' => ['newsplicity_front_page_settings'],
 		'fields' => [],
 	];
